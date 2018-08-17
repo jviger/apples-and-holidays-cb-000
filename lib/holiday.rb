52695994c8e40c48a314b283624d64ad2e291b7e
Jason Viger <jason.viger@gmail.com>
2018-08-17 06:31:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do | season, season_data|
-    puts season.upcase
+    puts season.capitalize
+    season.each do |holiday, holiday_array
+      puts "#{holiday.capitalie}: #{holiday_array.
 
 end
 
