b571d824ca0bfd15c9c181d0f4bbd65b336e9755
Jason Viger <jason.viger@gmail.com>
2018-08-17 06:32:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do | season, season_data|
     puts season.capitalize
-    season.each do |holiday, holiday_array
+    season.each do |holiday, holiday_array|
       puts "#{holiday.capitalie}: #{holiday_array.flatten}"
     end
   end
